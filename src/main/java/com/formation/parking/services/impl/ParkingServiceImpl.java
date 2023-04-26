f76f11441ae1d31210c1e973d00d5fc02471183f
HysteriaKa <karine2310@gmail.com>
2023-04-26 10:51:15 +0200

    implem in Service

---
+++ src/main/java/com/formation/parking/services/impl/ParkingServiceImpl.java
@@ -0,0 +1,30 @@
+package com.formation.parking.services.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.formation.parking.models.Parking;
+import com.formation.parking.services.ParkingService;
+
+@Service
+public class ParkingServiceImpl implements ParkingService {
+
+	@Override
+	public List<Parking> getListeParkings() {
+
+		Parking parkingTest = new Parking();
+		parkingTest.setNom("Parking de test");
+		parkingTest.setNbPlacesTotal(200);
+		parkingTest.setNbPlacesDispo(125);
+		parkingTest.setStatut("OUVERT");
+		parkingTest.setHeureMaj("20h15");
+
+		ArrayList<Parking> liste = new ArrayList<Parking>();
+		liste.add(parkingTest);
+
+		return liste;
+	}
+
+}
