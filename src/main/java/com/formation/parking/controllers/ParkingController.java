1df808d257d421157f060f8b429ed945ab24d62e
HysteriaKa <karine2310@gmail.com>
2023-04-26 09:09:29 +0200

    first commit

---
+++ src/main/java/com/formation/parking/controllers/ParkingController.java
@@ -0,0 +1,29 @@
+package com.formation.parking.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formation.parking.models.Parking;
+
+@RestController
+public class ParkingController {
+
+	@RequestMapping(path = "/api/parkings", method = RequestMethod.GET)
+	public List<Parking> getListeParkings() {
+		
+		/*Parking parkingTest = new Parking();
+		parkingTest.setNom("Parking de test");
+		parkingTest.setNbPlacesTotal(200);
+		parkingTest.setNbPlacesDispo(125);
+		parkingTest.setStatut("OUVERT");
+		parkingTest.setHeureMaj("20h15");
+		ArrayList<Parking> liste = new ArrayList<Parking>();
+		liste.add(parkingTest);*/
+
+		return liste;
+	}
+}
